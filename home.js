08d17cefacfb76e0b1d05eea9fc95023d1c6a07c
Benny Mendoza <bennybc9387@gmail.com>
2022-05-31 20:48:45 -0600

    missed a few comments

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 
 
-//Create references to the dropdown's
+
 const yearSelect = document.getElementById("year");
 const monthSelect = document.getElementById("month");
 const daySelect = document.getElementById("day");
@@ -12,7 +12,6 @@ const months = ['January', 'February', 'March', 'April',
 
 
 
-//Months are always the same
 (function populateMonths(){
     for(let i = 0; i < months.length; i++){
         const option = document.createElement('option');
@@ -25,14 +24,13 @@ const months = ['January', 'February', 'March', 'April',
 let previousDay;
 
 function populateDays(month){
-    //Delete all of the children of the day dropdown
-    //if they do exist
+    
     while(daySelect.firstChild){
         daySelect.removeChild(daySelect.firstChild);
     }
-    //Holds the number of days in the month
+
     let dayNum;
-    //Get the current year
+
     let year = yearSelect.value;
 
     if(month === 'January' || month === 'March' || 
@@ -43,14 +41,14 @@ function populateDays(month){
     || month === 'September' || month === 'November') {
         dayNum = 30;
     }else{
-        //Check for a leap year
+
         if(new Date(year, 1, 29).getMonth() === 1){
             dayNum = 29;
         }else{
             dayNum = 28;
         }
     }
-    //Insert the correct days into the day <select>
+
     for(let i = 1; i <= dayNum; i++){
         const option = document.createElement("option");
         option.textContent = i;
@@ -71,9 +69,9 @@ function populateDays(month){
 }
 
 function populateYears(){
-    //Get the current year as a number
+
     let year = new Date().getFullYear();
-    //Make the previous 100 years be an option
+
     for(let i = 0; i < 101; i++){
         const option = document.createElement("option");
         option.textContent = year - i;
